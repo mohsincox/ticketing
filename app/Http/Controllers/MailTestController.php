6a15ce3adf5a73c1569a0934759bbfe39407e607
Mohsin Iqbal <mohsincse2015@gmail.com>
2021-11-25 08:29:04 +0600

    Update MailTestController.php

--- app/Http/Controllers/MailTestController.php
+++ app/Http/Controllers/MailTestController.php
@@ -22,7 +22,7 @@ class MailTestController extends Controller
 		    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 		    $mail->SMTPAuth = true;                               // Enable SMTP authentication
 		    $mail->Username = 'mohsincse2015@gmail.com';                 // SMTP username
-		    $mail->Password = 'mohsincse123';                           // SMTP password
+		    $mail->Password = '************';                           // SMTP password
 		    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 		    $mail->Port = 587;                                    // TCP port to connect to
 
